db926d955e794bc4b0f68e247195c0360f3e1919
Emeline Floch <emeline.floch@gro-intelligence.com>
2023-12-19 10:45:31 -0500

    Fixed wrong output

--- run_bretagnizer.py
+++ run_bretagnizer.py
@@ -1,55 +1,8 @@
-import pandas as pd
-import constants as const
-import random
 import argparse
-import multilayer_perceptron as mlp
 
-
-def load_inputs(input_path):
-    """Load the inputs from the csv file into a list of names"""
-    df = pd.read_csv(input_path, sep=";")
-    inputs = list(df[const.COMMUNE_NAME_COL].values)
-    return inputs
-
-def get_vocab_and_size(input_texts):
-    """Get the vocabulary and its size from the input list of names"""
-    vocab = "".join(input_texts)
-    vocab = ['.'] + list(sorted(set(vocab)))
-    vocab_size = len(vocab)
-    return vocab, vocab_size
-
-
-def get_mappings(vocab):
-    """Get the mappings from characters to indices and vice-versa"""
-    char_to_idx = {char: i for i, char in enumerate(vocab)}
-    idx_to_char = {i: char for i, char in enumerate(vocab)}
-    return char_to_idx, idx_to_char
-
-def add_dots(input, n):
-    """Add dots at the beginning and end of each name"""
-    return "." * n + input + "."
-
-def build_n_grams(inputs, char_to_idx, n):
-    """Build n-grams from the input list of names. Return the n-grams and the next character in
-    two lists"""
-    n_grams_x = []
-    y = []
-    for input in inputs:
-        input = add_dots(input, n)
-        for i in range(len(input) - n):
-            n_gram = input[i:i + n]
-            print(n_gram, input[i + n])
-            n_grams_x.append([char_to_idx[char] for char in n_gram])
-            y.append(char_to_idx[input[i + n]])
-    return n_grams_x, y
-
-
-def get_batch_idx(train_x, train_y, batch_size):
-    """ Get a random batch of indices from the training data"""
-    batch_idx = random.sample(range(len(train_x)), batch_size)
-    train_batch_x = [train_x[i] for i in batch_idx]
-    train_batch_y = [train_y[i] for i in batch_idx]
-    return train_batch_x, train_batch_y
+import src.constants as const
+from src import multilayer_perceptron as mlp
+from src.utils import load_inputs, get_vocab_and_size, get_mappings, build_n_grams, get_batch_idx
 
 
 def run_bretagnizer(input_path,
